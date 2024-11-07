0f0c65532b2cab85838cb80da11f79cc1daf30a7
priyadarshini122 <149045801+priyadarshini122@users.noreply.github.com>
2024-11-07 11:28:31 +0530

    Update index.js
    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react/client'
-import App from './App'
 import {BrowserRouter} from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 )
